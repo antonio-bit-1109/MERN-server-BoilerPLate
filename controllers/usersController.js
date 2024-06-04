f5e9601edaff8c8fbdb7093e355d4f3b7fac3b2f
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-06-04 17:21:02 +0200

    get all users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,11 +2,21 @@ const user = require("../models/User");
 const note = require("../models/Note");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 //@desc get all users
 //route GET /users
 //@access Private
-const GetAllUsers = asyncHandler(async (req, res) => {});
+const GetAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find().select("-password").lean();
+
+    if (!users) {
+        return res.status(400).json({ message: "No users found." });
+    }
+    if (users) {
+        return res.json(users);
+    }
+});
 
 //@desc edit a user
 //route PATCH /users
