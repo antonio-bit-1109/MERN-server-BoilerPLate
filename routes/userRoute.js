e0a67c53c83a7bd0bb6c640d38f8cb3b68957b34
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-06-08 16:42:40 +0200

    middleware autenticazione

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const autenticateMiddleware = require("../middleware/auth");
 
 // una volta giunti in userRoute questa diventa la root "/" da cui reindirizzare al controller specifico. (get , post , put , delete)
 router
@@ -10,7 +11,7 @@ router
     .patch(usersController.EditUser)
     .delete(usersController.DeleteUser);
 
-router.route("/get-user").post(usersController.GetSingleUser);
+router.route("/get-user").post(autenticateMiddleware, usersController.GetSingleUser);
 router.route("/user-soft-delete").post(usersController.softDeleteUser);
 
 module.exports = router;
